e23415e4b90459492c4749497e3a0a3cc870571d
Eric Crosson <eric.s.crosson@utexas.edu>
2013-03-17 18:47:37 -0500

    Added separate slide pot's for RGB
    Not enough resolution with my current hardware for the entire spectrum
    on one slide.

--- ambient_lighting.ino
+++ ambient_lighting.ino
@@ -11,13 +11,14 @@
 #define greenPin  10		//Green pin 10 has PWM
 #define bluePin   11		//Blue pin 11 has PWM
 
-#define brightnessPin A0
-#define wavelengthPin A1
+#define adcPin    A0
+#define redAdc    A1
+#define greenAdc  A2
+#define blueAdc   A3
 
 bool down = true;		/* directional flag */
 
-uchar brightnessValue;
-uchar wavelengthValue;
+int adc;
 uchar red, green, blue;		//red, green and blue values
 
 void setup()
@@ -26,8 +27,10 @@ void setup()
     pinMode(greenPin, OUTPUT);
     pinMode(bluePin, OUTPUT);
 
-    digitalWrite(brightnessPin, HIGH);  // set pullup
-    digitalWrite(wavelengthPin, HIGH);  // set pullup
+    digitalWrite(adcPin, HIGH);	  // set pullup on analog pin 0
+    digitalWrite(redAdc, HIGH);	  // set pullup on analog pin 1
+    digitalWrite(blueAdc, HIGH);  // set pullup on analog pin 2
+    digitalWrite(greenAdc, HIGH); // set pullup on analog pin 3
 
     mapValueToPins(255); //run time test
 
@@ -38,81 +41,20 @@ void setup()
 
 void loop()
 {
-    /* writeToPins(50, 50, 255); */
+    adc = analogRead(adcPin);
+    /* mapValueToPins(adc / 4); */
 
-    brightnessValue = analogRead(brightnessPin) / 4;
-    /* Will be 0-255 (actually a little higher than 0).
-     * Now we will scale it to x-100 */
-    brightnessValue *= (100/255);
+    red = analogRead(redAdc) / 4;
+    green = analogRead(greenAdc) / 4;
+    blue = analogRead(blueAdc) / 4;
 
-    wavelengthValue = analogRead(wavelengthPin);
-    /* Normalizing from 0-1023 to 380-780 */
-    wavelengthValue *= (400/1023 + 380);
+    writeToPins(red, green, blue);
 
-    wavelengthToRGB(wavelengthValue, brightnessValue);
-
-    delay(DELAY); //to be safe
-}
-
-void wavelengthToRGB(int wavelength, uchar brightness)
-{
-    // color
-    if (wavelength >= 380 && wavelength < 440) {
-        red = -(wavelength - 440.) / (440. - 350.);
-        green = 0.0;
-        blue = 1.0;
-    }
-    else if (wavelength >= 440 && wavelength < 490) {
-	red = 0.0;
-	green = (wavelength - 440.) / (490. - 440.);
-	blue = 1.0;
-    }
-    else if (wavelength >= 490 && wavelength < 510) {
-	red = 0.0;
-	green = 1.0;
-	blue = -(wavelength - 510.) / (510. - 490.);
-    }
-    else if (wavelength >= 510 && wavelength < 580) {
-        red = (wavelength - 510.) / (580. - 510.);
-	green = 1.0;
-	blue = 0.0;
-    }
-    else if (wavelength >= 580 && wavelength < 645) {
-	red = 1.0;
-	green = -(wavelength - 645.) / (645. - 580.);
-	blue = 0.0;
-    }
-    else if (wavelength >= 645 && wavelength <= 780) {
-	red = 1.0;
-	green = 0.0;
-	blue = 0.0;
-    }
-    else {
-	red = 0.0;
-	green = 0.0;
-	blue = 0.0;
-    }
-
-    // intensity correction
-    double SSS;
-    if (wavelength >= 380 && wavelength < 420)
-    	SSS = 0.3 + 0.7*(wavelength - 350) / (420 - 350);
-    else if (wavelength >= 420 && wavelength <= 700)
-    	SSS = 1.0;
-    else if (wavelength > 700 && wavelength <= 780)
-    	SSS = 0.3 + 0.7*(780 - wavelength) / (780 - 700);
-    else
-    {
-    	SSS = 0.0;
-    	SSS *= 255;
-    }
-
-    SSS = 1;
-    writeToPins(SSS*red, SSS*green, SSS*blue);
+    delay(DELAY); //just to be safe
 }
 
 /* Analog for PWM */
-void writeToPins(int red, int green, int blue)
+void writeToPins(int red, int blue, int green)
 {
     analogWrite (redPin, red);
     analogWrite (greenPin, green);
