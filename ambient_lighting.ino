a6e3d8b7501e25b92b1fdaf8bde24c213dc2cc82
Eric Crosson <eric.s.crosson@utexas.edu>
2013-03-17 23:38:15 -0500

    Added overflow detection

--- ambient_lighting.ino
+++ ambient_lighting.ino
@@ -56,7 +56,18 @@ void loop()
 /* Analog for PWM */
 void writeToPins(int red, int blue, int green)
 {
+    keepColorsInBounds();
     analogWrite (redPin, red);
     analogWrite (greenPin, green);
     analogWrite (bluePin, blue);
+}
+
+void keepColorsInBounds()
+{
+    if (red < 0)          red	= 0;
+    else if (red > 255)   red	= 255;
+    if (green < 0)        green = 0;
+    else if (green > 255) green = 255;
+    if (blue < 0)         blue	= 0;
+    else if (blue > 255)  blue	= 255;
 }
